81da7d780fcfe368ae8936775fb921f7a6d27d19
Rem Kim <rkim@wealthsimple.com>
2026-01-28 14:08:24 -0500

    nvim: switch to ruby-lsp, fix keymaps timing
    - replace solargraph with ruby-lsp (better Rails support)
    - move LspAttach autocmd before plugin load to fix keymap registration
    - use mise ruby path for ruby-lsp cmd

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,23 @@
+-- Keymaps on LSP attach (register early, before any LSP loads)
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local map = function(keys, func, desc)
+      vim.keymap.set("n", keys, func, { buffer = args.buf, desc = desc })
+    end
+
+    map("gd", vim.lsp.buf.definition, "Go to definition")
+    map("gr", vim.lsp.buf.references, "Find references")
+    map("gD", vim.lsp.buf.declaration, "Go to declaration")
+    map("gI", vim.lsp.buf.implementation, "Go to implementation")
+    map("K", vim.lsp.buf.hover, "Hover documentation")
+    map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+    map("<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+    map("[d", vim.diagnostic.goto_prev, "Previous diagnostic")
+    map("]d", vim.diagnostic.goto_next, "Next diagnostic")
+    map("<leader>d", vim.diagnostic.open_float, "Show diagnostic")
+  end,
+})
+
 return {
   -- Mason: LSP server installer
   {
@@ -28,7 +48,10 @@ return {
       vim.lsp.config("pyright", { capabilities = capabilities })
       vim.lsp.config("gopls", { capabilities = capabilities })
       vim.lsp.config("rust_analyzer", { capabilities = capabilities })
-      vim.lsp.config("solargraph", { capabilities = capabilities })
+      vim.lsp.config("ruby_lsp", {
+        capabilities = capabilities,
+        cmd = { "/Users/rkim/.local/share/mise/installs/ruby/3.4.7/bin/ruby-lsp" },
+      })
       vim.lsp.config("lua_ls", {
         capabilities = capabilities,
         settings = {
@@ -46,32 +69,21 @@ return {
           "pyright",
           "gopls",
           "rust_analyzer",
-          "solargraph",
           "lua_ls",
         },
         automatic_installation = true,
-        automatic_enable = true,
       })
 
-      -- Keymaps on LSP attach
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = args.buf, desc = desc })
-          end
-
-          map("gd", vim.lsp.buf.definition, "Go to definition")
-          map("gr", vim.lsp.buf.references, "Find references")
-          map("gD", vim.lsp.buf.declaration, "Go to declaration")
-          map("gI", vim.lsp.buf.implementation, "Go to implementation")
-          map("K", vim.lsp.buf.hover, "Hover documentation")
-          map("<leader>ca", vim.lsp.buf.code_action, "Code action")
-          map("<leader>rn", vim.lsp.buf.rename, "Rename symbol")
-          map("[d", vim.diagnostic.goto_prev, "Previous diagnostic")
-          map("]d", vim.diagnostic.goto_next, "Next diagnostic")
-          map("<leader>d", vim.diagnostic.open_float, "Show diagnostic")
-        end,
+      -- Enable LSP servers
+      vim.lsp.enable({
+        "ts_ls",
+        "pyright",
+        "gopls",
+        "rust_analyzer",
+        "ruby_lsp",
+        "lua_ls",
       })
+
     end,
   },
 }
